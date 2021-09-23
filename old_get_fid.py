39b4fba84187e05fd5e6bff62a40c24f53284895
Tori.Zoo <zoosecretbase.chicken@gmail.com>
2021-09-24 00:54:11 +0900

    add requirement files

---
+++ old_get_fid.py
@@ -0,0 +1,17 @@
+from test_fid_score import *
+from real_imgs import *
+
+airplane = 0
+automobile = 0
+bird = 1
+cat = 0
+deer = 0
+dog = 0
+frog = 1
+horse = 0
+ship = 0
+truck = 0
+
+real_imgs_list = real_imgs_row_data(airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck)
+a_real_img = a_real_img(real_imgs_list)
+
